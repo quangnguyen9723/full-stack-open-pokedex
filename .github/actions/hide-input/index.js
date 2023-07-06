0eb3daf172c24808461bdd728302d52649a82537
quangnguyen9723 <78969220+quangnguyen9723@users.noreply.github.com>
2023-07-06 11:55:15 -0700

    test

--- .github/actions/hide-input/index.js
+++ .github/actions/hide-input/index.js
@@ -1,15 +1,31 @@
-const core = require('@actions/core');
+import core from '@actions/core';
+import fs from 'fs';
+import colors from 'ansi-styles';
 
-try {
-  const inputsToHide = core.getInput('inputs-to-hide');
-  const inputs = inputsToHide.split(',');
+// Get a list of inputs that we should hide the values of. 
 
-  // Hide the specified inputs
-  for (const input of inputs) {
-    core.exportVariable(input, '');
-  }
+const listOfInputsToExclude = core.getInput('exclude_inputs').split(',').map(item => item.trim());
+let inputsObject = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')).inputs;
+let allInputKeys = Object.keys(inputsObject);
 
-  console.log('Inputs hidden successfully!');
-} catch (error) {
-  core.setFailed(`Action failed with error: ${error}`);
+core.info(`${colors.blue.open}All inputs for this workflow: ${allInputKeys}`)
+core.info(`${colors.blue.open}List of inputs to exclude: ${listOfInputsToExclude}`)
+
+const inputKeysToHide = allInputKeys.filter(inputKey => !listOfInputsToExclude.includes(inputKey))
+
+core.info(`${colors.yellow.open}After removing the inputs to exclude, these are all of the inputs that will be hidden: ${inputKeysToHide}`)
+
+// Time to hide the values 
+
+core.info('')
+
+for (const inputKey of inputKeysToHide) {    
+  core.info(`${colors.white.open}Hiding value for input: ${inputKey}`)
+  core.setSecret(inputsObject[inputKey])
 }
+
+core.info('')
+
+// Done! 
+
+core.info(`${colors.green.open}Done!`)
